9c2a426774a2d1e27e602742742fca3507b98c11
SuprasannaBhaumik <suprasanna.bhaumik86@gmail.com>
2018-09-17 15:18:08 +0530

    Initial commit

---
+++ src/com/study/ExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.study;
+
+public class ExceptionHandler {
+
+	public static String getName() {
+		System.out.println("1");
+		try {
+			String s1 = null;
+			s1.toString();		
+			System.out.println("2");
+			return "2";
+		} catch(Exception e) {
+			System.out.println("3");
+			return "3";
+		} finally {
+			System.out.println("4");
+			//any return from finally block will be overriden
+			//return "4";
+		}
+	}
+	
+	public static void main(String s[]) {
+		System.out.println(getName());
+	}
+}
